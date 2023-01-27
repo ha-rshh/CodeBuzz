23630d6ec36c223994d6403586ed754358f91b6e
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-01-28 00:18:32 +0530

    added main.js

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-function preview() {
-  let content = "";
-  content +=
-    "<style>" + document.querySelector(".cssEditor").innerText + "</style>";
-  content +=
-    "<script>" + document.querySelector(".jsEditor").innerText + "</script>";
-  content +=
-    "<body>" + document.querySelector(".htmlEditor").innerText + "</body>";
 
-    document.getElementById("preview").src = "data:text/html; charset=UTF-8, <html>" + content + "<\/html>";
+
+
+
+const htmlCode = document.getElementById("html-code");
+const cssCode = document.getElementById("css-code");
+const jsCode = document.getElementById("js-code");
+const result = document.getElementById("result");
+
+function run() {
+  localStorage.setItem("htmlCode", htmlCode.value);
+  localStorage.setItem("cssCode", cssCode.value);
+  localStorage.setItem("jsCode", jsCode.value);
+  result.contentDocument.body.innerHTML = `<style> ${localStorage.cssCode} </style>` + localStorage.htmlCode;
+  result.contentWindow.eval(localStorage.jsCode);
 }
 
+htmlCode.onkeyup = () => run();
+cssCode.onkeyup = () => run();
+jsCode.onkeyup = () => run();
+
+htmlCode.value = localStorage.htmlCode;
+cssCode.value = localStorage.cssCode;
+jsCode.value = localStorage.jsCode;
