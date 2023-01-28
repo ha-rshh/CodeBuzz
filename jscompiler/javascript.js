82548ac21cf0d40a705f1934422908be344b3c85
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-01-28 20:33:54 +0530

    new compiler

--- jscompiler/javascript.js
+++ jscompiler/javascript.js
@@ -1,20 +1,33 @@
 const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   mode: "text/javascript",
-  theme: "material-darker",
+  // theme: "material-darker",
   lineNumbers: true,
   autoCloseBrackets: true,
+  closeTags: true,
 });
-
+console.oldLog = console.log;
+console.log = function(value) {
+  // console.oldLog(value);
+  return value;
+}
+const outputScreen = CodeMirror.fromTextArea(document.getElementById("result"), {lineNumbers: false})
 const width = window.innerWidth;
 editor.setSize(0.45 * width, "550");
-console.log("hello");
+// console.log("hello");
 
 const input = document.getElementById("user-input");
+const result = document.getElementById("result");
 
 const [run, clear] = document.querySelectorAll("button");
 
 run.addEventListener("click", () => {
-    const codeRun = editor.getValue();
-    input.innerHTML = (eval(`${codeRun}`));
+  const codeRun = editor.getValue();
+  // result.innerHTML = codeRun;
+  //outputScreen.setValue(eval(`${codeRun}`));
+  outputScreen.setValue(eval(`${codeRun}`));
+
 });
 // clear.addEventListener("click", (_) => input.setValue(""));
+//if(codeRun) {
+ // input.innerHTML = (eval(`${codeRun}`));
+//}
