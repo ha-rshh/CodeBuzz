456b8fad9f0e316aa9abe4cba82d0fa5efb6f43a
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-01-29 15:01:08 +0530

    added sidenav

--- jscompiler/javascript.js
+++ jscompiler/javascript.js
@@ -1,33 +1,71 @@
-const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-  mode: "text/javascript",
-  theme: "monokai",
-  lineNumbers: true,
-  autoCloseBrackets: true,
-  closeTags: true,
-});
+window.onload = () => {
+  const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    value: "function myScript(){return 100;}\n",
+    mode: "text/javascript",
+    theme: "monokai",
+    lineNumbers: true,
+    autoCloseBrackets: true,
+    closeTags: true,
+    lineWrapping: true,
+  });
 
+  const output = CodeMirror.fromTextArea(document.getElementById("result"), {
+    theme: "monokai",
+    lineNumbers: false,
+    readOnly: true,
+    
+  });
 
-console.oldLog = console.log;
-console.log = function (value) {
-  console.oldLog(value);
-  return value;
+  const run = document.querySelector("#run-btn");
+  const clear = document.querySelector("#clear-btn");
+
+  run.addEventListener("click", () => {
+    const runCode = editor.getValue();
+    try {
+      output.replaceRange(
+        `$ ` + eval(`${runCode}`) + "\n",
+        CodeMirror.Pos(output.lastLine())
+      );
+    } catch (e) {
+      output.replaceRange(`$ ` + e + "\n", CodeMirror.Pos(output.lastLine()));
+    }
+  });
+  clear.addEventListener("click", () => {
+    output.setValue("");
+  });
 };
 
+submit(function(e){
+   var value = editor.getValue();
+   if(value. length == 0){
+     alert("Missing comment!");
+     } 
+    });
+// run.addEventListener("click", () => {
+//   const runCode = editor.getValue();
+// clear.addEventListener("click", () => {
+//    output.setValue("")
+//    console.log(output.value)
+//   }
+// try {
+//
+// } catch (e) {
+//
+//   });
 
-const outputScreen = document.getElementById("result")
+// run.addEventListener("click", () => {
+//   const runCode = editor.getValue();
+//   output.setValue(eval(`${runCode}`));
+//   //console.log(ans())
+//   // outputScreen.innerText += ans;
+// });
 
-const input = document.getElementById("user-input");
-const result = document.getElementById("result");
+// clear.addEventListener("click", () => {
+//   outputScreen.setValue("");
+// });
 
-const [run, clear] = document.querySelectorAll("button");
-
-run.addEventListener("click", () => {
- const codeRun = editor.getValue();
- const ans =  eval(codeRun)
-  //console.log(ans())
-  outputScreen.innerText += ans;
-});
-
-clear.addEventListener("click", () => {
-  outputScreen.setValue("");
-});
+// console.oldLog = console.log;
+// console.log = function (value) {
+//   console.oldLog(value);
+//   return value;
+// };
--- main.js
+++ main.js
@@ -56,3 +56,14 @@ jsCode.on("change", updatePreview);
 // htmlCode.getValue() = localStorage.htmlCode;
 // cssCode.getValue() = localStorage.cssCode;
 // jsCode.getValue() = localStorage.jsCode;
+
+// x navbar
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.overlay');
+
+menu_btn.addEventListener('click', function(){
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    overlay.classList.toggle('is-overlay-active');
+})
